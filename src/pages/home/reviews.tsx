05a750641e4c3dc53eb3c22721c8c2ce62044558
liteboho moalosi <litebohojamesmoalosi@gmail.com>
2024-07-27 12:09:45 +0200

    update portfolio

--- src/pages/home/reviews.tsx
+++ src/pages/home/reviews.tsx
@@ -12,7 +12,7 @@ export type ReviewType = {
 export const reviews: ReviewType[] = [
     {
         name: "Senate Lerotholi",
-        status: ["Journalist@Lilapalapha TV", "Freelance Writer", "Marketing Director"],
+        status: ["Journalist", "Freelance Writer", "Marketing Director"],
         profileLink: "mailto:lerotholisenate300@gmail.com",
         body: "This is exceptionally beautiful. You are amazingly good. I'm so glad I had to work with you. Keep up the status quo!",
         img: senateLerotholi,
